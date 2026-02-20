6866f8e53bc17fb5b29e55c79c485af724827d46
Megi <megibego28@gmail.com>
2026-02-20 13:26:42 +0000

    Initial version of basic player movements

---
+++ src/entities/Player.js
@@ -0,0 +1,83 @@
+//is this correct?
+import {HandleInput} from "../systems/HandleInput.js";
+
+export class Player{
+  
+  constructor(ground){
+      //Placeholder player object 
+      this.side=50;
+      this.colour=color(255, 192,203);
+      //
+      this.posX=0;
+      this.posY=ground-this.side;
+      this.speedX=10; //width%speedX needs to be 0
+      this.speedY=0;
+      this.gravity=0.8;
+      this.ground=ground;
+      this.defaultJumpSpeed=12;
+   }
+  
+  //also provisional
+  drawPlayer(){
+      fill(this.colour);
+      noStroke();
+      square(this.posX, this.posY, this.side);
+   }
+  
+  //move 
+   movePlayer(){ 
+      let keys=playerInput.getLast2Pressed();
+      for(let k of keys){
+         if (k==='a'){
+            if(this.posX<=0){
+               this.posX-=0;
+            }
+            else {this.posX-=this.speedX;
+            }
+         }
+         else if(k==='d'){
+            if (this.posX>=width-this.side){
+               this.posX+=0;
+            }
+            else{this.posX+=this.speedX;
+            }
+         }
+         else{
+            this.posX+=0;
+            this.posY+=0;
+         }
+         
+      }
+
+   }
+  
+   jumpUp(){
+      let keys=HandleInput.getLast2Pressed();
+      for(let k of keys){
+         if (k==='w' && this.posY>=this.ground){
+         if(this.posY<=0){
+            this.posY-=0;
+         }
+         else{
+            this.speedY=-this.defaultJumpSpeed;
+            this.speedY-=this.gravity;
+            this.posY+=this.speedY;
+            
+            }
+         }
+      }
+   }
+  
+   comeDown(){
+      this.speedY += this.gravity;  
+      this.posY += this.speedY;    
+
+      if(this.posY >= this.ground){
+         this.posY = this.ground;
+         this.speedY = 0;
+      }
+   }
+
+   
+
+}
---
+++ src/systems/HandleInput.js
@@ -0,0 +1,47 @@
+
+export class HandleInput {
+   constructor(validKeys) {
+      this.validKeys = validKeys;
+      this.pressedKeys = [];
+   }
+
+   handleKeyPressed(k) {
+      k = k.toLowerCase();
+      this.pressedKeys.push(k);
+   }
+
+   handleKeyReleased(k) {
+      k = k.toLowerCase();
+      const index = this.pressedKeys.indexOf(k);
+      if (index !== -1) {
+         this.pressedKeys.splice(index, 1);
+      }
+   }
+
+   //getLastPressed() {
+   //   return this.pressedKeys[this.pressedKeys.length - 1];
+   //}
+  
+   getLast2Pressed() {
+      const len = this.pressedKeys.length;
+      if (len >= 2) {
+         return [this.pressedKeys[len - 2], this.pressedKeys[len - 1]];
+      } 
+      else if (len === 1) {
+         return [this.pressedKeys[0]];
+      } 
+      else {
+         return [];
+      }
+   }
+
+}
+
+function keyPressed(event) {
+  playerInput.handleKeyPressed(event.key);
+}
+
+function keyReleased(event) {
+  playerInput.handleKeyReleased(event.key);
+}
+
