25e6d06f08e3fbbc01ed477303a7795abcf00d79
liilee111 <jacqueline891008@gmail.com>
2026-04-17 22:43:01 +0100

    remove text on characters

--- src/config/AnimationConfigBunny.js
+++ src/config/AnimationConfigBunny.js
@@ -1,7 +1,7 @@
 export const AnimationConfigBunny = {
     IDLE: [0, 1],
     RUN: [3, 4, 5],
-    JUMP: [10, 11],
-    FALL: [11, 12],
-    RESPAWNING: [8],
+    JUMP: [12, 13],
+    FALL: [13, 12],
+    RESPAWNING: [9],
 };
